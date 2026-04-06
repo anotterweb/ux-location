8569f2214dc9f3e8b5f03d7538f45d9afc72a01b
Luc Hidalgo <luchidalgo@gmail.com>
2026-04-06 17:45:44 +0200

    Update form theme and initial center

--- assets/dist/controller.js
+++ assets/dist/controller.js
@@ -18,20 +18,40 @@ export default class extends Controller {
     };
 
     connect() {
+        let initialLat = this.latValue;
+        let initialLng = this.lngValue;
+        const inputValue = this.inputTarget.value;
+        if (inputValue) {
+            const parts = inputValue.split(',');
+            if (parts.length === 2 && !isNaN(parts[0]) && !isNaN(parts[1])) {
+                initialLat = parseFloat(parts[0].trim());
+                initialLng = parseFloat(parts[1].trim());
+                hasMarker = true;
+            }
+        }
+
         mapboxgl.accessToken = this.tokenValue;
         this.map = new mapboxgl.Map({
             container: this.mapTarget,
             style: this.styleValue,
-            center: [this.lngValue, this.latValue],
+            center: [initialLng, initialLat],
             zoom: this.zoomValue
         });
 
+        if (hasMarker) {
+            this.marker = new mapboxgl.Marker().setLngLat([initialLng, initialLat]).addTo(this.map);
+        }
+
         this.map.on('click', (e) => {
             if (this.marker) {
-                this.marker.setLngLat([e.lngLat.lng, e.lngLat.lat]);
+                this.marker.setLngLat(e.lngLat);
             } else {
-                this.marker = new mapboxgl.Marker().setLngLat([e.lngLat.lng, e.lngLat.lat]).addTo(this.map);
+                this.marker = new mapboxgl.Marker().setLngLat(e.lngLat).addTo(this.map);
             }
+
+            const newValue = `${e.latlng.lat},${e.latlng.lng}`;
+            this.inputTarget.value = newValue;
+            this.inputTarget.dispatchEvent(new Event('change', { bubbles: true }));
         });
     }
 
